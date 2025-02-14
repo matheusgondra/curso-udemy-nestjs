c9f8fefb344a0347cd412e3bf9cd8126ee11830e
matheusgondra <matheusgondra951@gmail.com>
2025-02-14 07:22:52 -0300

    criando interceptor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
-import { UpdateUserDTO } from "./dto/update-user.dto";
 import { UpdatePartialUserDTO } from "./dto/update-partial-user.dto";
+import { UpdateUserDTO } from "./dto/update-user.dto";
 import { UserService } from "./user.service";
 
 @Controller("users")
