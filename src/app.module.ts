69ccee96420d8aca252239874305e3501b24d84b
matheusgondra <matheusgondra951@gmail.com>
2025-02-24 18:14:04 -0300

    proteção contra ataque de força bruta

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { forwardRef, Module } from "@nestjs/common";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 100
+      }
+    ])
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
 })
 export class AppModule {}
